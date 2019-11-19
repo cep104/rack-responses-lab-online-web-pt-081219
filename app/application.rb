8d309355c788c1d8b5e6c9d37e23619c1f0f2829
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-19 03:35:06 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+def call(env)
+    resp = Rack::Response.new	    
+    if 
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+    
+    end
+
